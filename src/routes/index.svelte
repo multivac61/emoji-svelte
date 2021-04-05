064a09173834c84edea6b296a204aa6d7a5f905a
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2021-04-05 10:58:51 +0000

    Adding game logic and fix spelling mistakes in movies

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,13 @@
 </script>
 
 <script>
+	function shuffle(array) {
+		for (var i = 0; i < array.length - 1; i++) {
+			const j = i + Math.floor(Math.random() * (array.length - i));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	}
 	const movies = {
 		'👦👴🚗⏱': 'back to the future',
 		'👸🌹🐻': 'beauty and the beast',
@@ -14,12 +21,12 @@
 		'⚡️👦🔨': 'thor',
 		'👨🏻🎤': 'bohemian rhapsody',
 		'🐼👊': 'kung fu panda',
-		'👦💍➡️🌋': 'lord of the rings',
+		'👦💍➡️🌋': 'the lord of the rings',
 		'👽📞👦🚲🌕': 'et',
 		'🍴🙏❤️': 'eat, pray, love',
-		'👑💬🎙': 'the kings speech',
+		'👑💬🎙': "the king's speech",
 		'🐍✈️': 'snakes on a plane',
-		'🐛🐜🐞': 'a bugs life',
+		'🐛🐜🐞': "a bug's life",
 		'📱🍎': 'jobs',
 		'⏰🔧🍊': 'a clockwork orange',
 		'💍📺': 'the ring',
@@ -29,47 +36,45 @@
 		'🛳🧊': 'titanic'
 	};
 
-	let guess = '';
-	let answer = undefined;
-	let emoji = '⚡️👦🔨';
+	let random_keys = shuffle(Object.keys(movies));
 
-	function random_emoji() {
-		const keys = Object.keys(movies);
-		return keys[Math.floor(Math.random() * keys.length)];
-	}
+	let [guess, answer, index] = ['', undefined, 0];
 
 	function submit_guess() {
-		answer = guess.toLowerCase().trim() === movies[emoji];
+		answer = guess.toLowerCase().trim() === movies[random_keys[index]];
 		if (answer === true) {
-			guess = '';
-			emoji = random_emoji();
+			[guess, index] = ['', index + 1];
 		}
 	}
 </script>
 
 <main class="flex flex-col items-center justify-center min-h-screen text-center">
-	<h1 class="m-2 text-3xl py-7 md:text-6xl">Guess the 🎞</h1>
-	<h1 class="m-2 text-5xl py-7 tracking-[0.2em] md:text-9xl ">{emoji}</h1>
-	<div class="flex item-center px-8 mt-10 text-gray-700">
-		<form on:submit|preventDefault={submit_guess}>
-			<input
-				bind:value={guess}
-				placeholder="Which movie?"
-				class="p-2 text-center bg-gray-100 text-gray-600 m-2"
-			/>
-			<button class="px-8 p-2 text-xl font-bold">Guess</button>
-		</form>
-	</div>
-	<div class="m-8 p-1">
-		{#if answer == undefined}
-			<p class="text-gray-600">You can do it 💪</p>
-			<p class="text-white">!</p>
-		{:else if answer == true}
-			<p class="text-green-600">Correct ☑️</p>
-			<p class="text-gray-200">Now try this one 🙈</p>
-		{:else}
-			<p class="text-red-600">Incorrect 🚽</p>
-			<p class="text-gray-200">Try Again...</p>
-		{/if}
-	</div>
+	{#if index === random_keys.length}
+		<h1 class="m-2 text-8xl py-7 md:text-8xl text-green-600">You win! 🎉</h1>
+	{:else}
+		<h1 class="m-2 text-3xl py-7 md:text-6xl">Guess the 🎞</h1>
+		<h1 class="m-2 text-5xl py-7 tracking-[0.2em] md:text-9xl ">{random_keys[index]}</h1>
+		<div class="flex item-center px-8 mt-10 text-gray-700">
+			<form on:submit|preventDefault={submit_guess}>
+				<input
+					bind:value={guess}
+					placeholder="Which movie?"
+					class="p-2 text-center bg-gray-100 text-gray-600 m-2"
+				/>
+				<button class="px-8 p-2 text-xl font-bold">Guess</button>
+			</form>
+		</div>
+		<div class="m-8 p-1">
+			{#if answer == undefined}
+				<p class="text-gray-600">You can do it 💪</p>
+				<p class="text-white">!</p>
+			{:else if answer == true}
+				<p class="text-green-600">Correct ☑️</p>
+				<p class="text-gray-200">Now try this one 🙈</p>
+			{:else}
+				<p class="text-red-600">Incorrect 🚽</p>
+				<p class="text-gray-200">Try Again...</p>
+			{/if}
+		</div>
+	{/if}
 </main>
