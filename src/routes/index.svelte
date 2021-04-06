b1b8585ceb22ba2a975fffa59dde1ef9c2829fe5
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2021-04-06 18:10:56 +0000

    Make prettier and accept more answers

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,69 +11,81 @@
 		return array;
 	}
 	const movies = {
-		'👦👴🚗⏱': 'back to the future',
-		'👸🌹🐻': 'beauty and the beast',
-		'🌃🇫🇷🗼': 'midnight in paris',
-		'👸🐸👑': 'princess and the frog',
-		'5️⃣0️⃣0️⃣☀️❤️': '500 days of summer',
-		'👨🏻👨🏻❤️🗻': 'brokeback mountain',
-		'🐳➡🌊': 'free willy',
-		'⚡️👦🔨': 'thor',
-		'👨🏻🎤': 'bohemian rhapsody',
-		'🐼👊': 'kung fu panda',
-		'👦💍➡️🌋': 'the lord of the rings',
-		'👽📞👦🚲🌕': 'et',
-		'🍴🙏❤️': 'eat, pray, love',
-		'👑💬🎙': "the king's speech",
-		'🐍✈️': 'snakes on a plane',
-		'🐛🐜🐞': "a bug's life",
-		'📱🍎': 'jobs',
-		'⏰🔧🍊': 'a clockwork orange',
-		'💍📺': 'the ring',
-		'👦✂️🙌': 'edward scissorhands',
-		'🌍🙈🙊🙉': 'planet of the apes',
-		'👦🍫🏭': 'charlie and the chocolate factory',
-		'🛳🧊': 'titanic'
+		'👦👴🚗⏱': ['back to the future'],
+		'👸🌹🐻': ['beauty and the beast'],
+		'🌃🇫🇷🗼': ['midnight in paris', 'a midnight in paris'],
+		'👸🐸👑': ['princess and the frog', 'the princess and the frog'],
+		'5️⃣0️⃣0️⃣☀️❤️': ['500 days of summer'],
+		'👨🏻👨🏻❤️🗻': ['brokeback mountain'],
+		'🐳➡🌊': ['free willy'],
+		'⚡️👦🔨': ['thor'],
+		'👨🏻🎤': ['bohemian rhapsody'],
+		'🐼👊': ['kung fu panda'],
+		'👦💍➡️🌋': ['lord of the rings', 'the lord of the rings'],
+		'👽📞👦🚲🌕': ['et', 'E.T. the Extra-Terrestrial'],
+		'🍴🙏❤️': ['eat, pray, love'],
+		'👑💬🎙': ['the king\'s speech', 'king\'s speech', 'kings speech'],
+		'🐍✈️': ['snakes on a plane'],
+		'🐛🐜🐞': ['a bug\'s life', "bugs life", 'bug\'s life'],
+		'📱🍎': ['jobs'],
+		'⏰🔧🍊': ['a clockwork orange', 'clockwork orange'],
+		'💍📺': ['the ring', 'ring'],
+		'👦✂️🙌': ['edward scissorhands'],
+		'🌍🙈🙊🙉': ['planet of the apes', 'the planet of the apes'],
+		'👦🍫🏭': ['charlie and the chocolate factory'],
+		'🛳🧊': ['titanic']
 	};
 
-	let random_keys = shuffle(Object.keys(movies));
+	let random_keys = [' '];
 
-	let [guess, answer, index] = ['', undefined, 0];
+	function foo() {
+		random_keys = shuffle(Object.keys(movies));
+	}
+
+	let [index, answer, guess] = [0, undefined, ""];
 
 	function submit_guess() {
-		answer = guess.toLowerCase().trim() === movies[random_keys[index]];
+		const my_guess = guess.toLowerCase().trim();
+		answer = movies[random_keys[index]].includes(my_guess);
+		console.log(my_guess, answer);
 		if (answer === true) {
 			[guess, index] = ['', index + 1];
 		}
 	}
+	function handleClick() {
+		[guess, index, answer] = ['', index + 1, true];
+	}
 </script>
 
-<main class="flex flex-col items-center justify-center min-h-screen text-center">
+<main
+	class="flex flex-col items-center justify-center min-h-screen text-center bg-gray-50"
+	on:loadeddata={foo()}
+>
 	{#if index === random_keys.length}
 		<h1 class="m-2 text-8xl py-7 md:text-8xl text-green-600">You win! 🎉</h1>
 	{:else}
-		<h1 class="m-2 text-3xl py-7 md:text-6xl">Guess the 🎞</h1>
-		<h1 class="m-2 text-5xl py-7 tracking-[0.2em] md:text-9xl ">{random_keys[index]}</h1>
-		<div class="flex item-center px-8 mt-10 text-gray-700">
+		<h1 class="m-2 font-semibold md:text-7xl text-4xl py-7 bg-clip-text text-transparent bg-gradient-to-l from-pink-500 via-red-500 to-yellow-500">Guess the movie</h1>
+
+		<h1 class="m-2 text-5xl py-7 tracking-[0.2em] md:text-9xl">{random_keys[index]}</h1>
+		<div class="flex item-center px-8 mt-10">
 			<form on:submit|preventDefault={submit_guess}>
 				<input
 					bind:value={guess}
 					placeholder="Which movie?"
-					class="p-2 text-center bg-gray-100 text-gray-600 m-2"
+					class="p-2 text-center bg-gray-100 text-gray-600 m-2 rounded-xl shadow-md"
 				/>
-				<button class="px-8 p-2 text-xl font-bold">Guess</button>
+				<button class="px-8 p-2 hover:pt-2 text-white bg-gradient-to-l from-pink-500 via-red-500 to-yellow-500 hover:from-red-500 
+				rounded-xl shadow-md">Guess</button>
 			</form>
 		</div>
 		<div class="m-8 p-1">
+			<p class="text-gray-600">You have guessed {index}/{random_keys.length}</p>
 			{#if answer == undefined}
 				<p class="text-gray-600">You can do it 💪</p>
-				<p class="text-white">!</p>
 			{:else if answer == true}
 				<p class="text-green-600">Correct ☑️</p>
-				<p class="text-gray-200">Now try this one 🙈</p>
-			{:else}
-				<p class="text-red-600">Incorrect 🚽</p>
-				<p class="text-gray-200">Try Again...</p>
+			{:else if answer == false}
+				<button class="text-red-600" on:click="{handleClick}">I give up🤦‍♂ ️Skip to next movie.</button>
 			{/if}
 		</div>
 	{/if}
